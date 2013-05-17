a1816d3ac894f20a1285f26edb8eabec9d473f80
Nicholas Crandall <ncrandall@gmail.com>
2013-05-17 12:16:02 -0700

    modify twilio worker to use an application_sid

--- app/workers/twilio_worker.rb
+++ app/workers/twilio_worker.rb
@@ -1,12 +1,15 @@
 class TwilioWorker
 	def make_call
-		#provider = Provider.first
-		phone = '801-508-4604'
+		provider = Provider.first
 
 		client = Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"])
 		count = 0
 
 		account = client.account
-		call = account.calls.create('+14155150551', phone, "http://www.bluemoonlist.com/microposts/dialback.xml?phone_user=#{phone}&count=#{count}")
+		call = account.calls.create({
+			from: '+14155150551', 
+			to: provider.phone, 
+			application_sid: "AP0b3c40b45634d668245976268b23b7c2" 
+		})
 	end
 end
