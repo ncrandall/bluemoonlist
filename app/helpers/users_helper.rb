34abf4933ceb511d0c1ad4a2637bcf489756092f
Nicholas Crandall <ncrandall@gmail.com>
2013-06-28 13:06:57 -0700

    use image_urls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+	def profile_image(user)
+		url = 'no_pic.jpg'
+		if !user.image_url.nil?
+			url = user.image_url
+		end
+
+		image_tag url, height: 50, width: 50
+	end
+
+	def large_profile_image(user)
+		url = 'no_pic.jpg'
+		if !user.image_url.nil?
+			tmp = URI::parse(user.image_url)
+			tmp.query = "type=large"
+			url = tmp.to_s
+		end
+		image_tag(url)
+	end
 end
